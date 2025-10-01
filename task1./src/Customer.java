c7c4cc0a8dddb693aef74b83a27e75981135625d
Melissabekirova04 <melissa.melike.bekirova@gmail.com>
2025-10-01 13:33:32 +0200

    Initial commit

---
+++ task1./src/Customer.java
@@ -0,0 +1,63 @@
+public class Customer {
+    private String firstName;
+    private String lastName;
+    private String username;
+    private int id;
+
+
+    //tæller op for hver ny kunde
+    private static int counter = 0;
+
+    public Customer(String firstName,String lastName,String username){
+        this.firstName=firstName;
+        this.lastName=lastName;
+        this.username=username;
+
+        counter++;
+        this.id = counter;
+    }
+
+    public String getFirstName(){
+        return firstName;
+    }
+
+    public void setFirstName(String firstName){
+        this.firstName=firstName;
+    }
+
+    public String getLastName(){
+        return lastName;
+    }
+
+    public void setLastName(String lastName){
+        this.lastName=lastName;
+    }
+
+    public String getUsername(){
+        return username;
+    }
+
+    public void setUsername(String username){
+        this.username=username;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String toString(){
+        return "Customer{"+
+                "id="+ id +
+                ", firstName='" + firstName +'\''+
+                ", lastName='" + lastName + '\'' +
+                ", username='" + username + '\'' +
+                '}';
+    }
+
+}
+
+
+
+
+
+
---
+++ task1./src/Main.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args){
+        ArrayList<Customer> customers=new ArrayList<>();
+
+        // Tilføj kunder (enten med variabler eller direkte i listen)
+        customers.add(new Customer("Peter", "Hansen", "peterh"));
+        customers.add(new Customer("Maria", "Jensen", "mariaj"));
+        customers.add(new Customer("Jonas", "Nielsen", "jonasn"));
+
+        // Kald printCustomers-metoden
+        printCustomers(customers);
+
+    }
+    // Statisk metode til at printe kunder
+    public static void printCustomers(ArrayList<Customer> customers) {
+        for (Customer c : customers) {
+            System.out.println("ID: " + c.getId() +
+                    ", Name: " + c.getFirstName() + " " + c.getLastName() +
+                    ", Username: " + c.getUsername());
+        }
+    }
+
+}
